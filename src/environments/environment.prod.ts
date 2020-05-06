0abd881044b518b454ac606a46a1fbb4d5a02447
Xavier Guinto Rios <xavierguinto3@outlook.es>
2020-05-06 16:29:47 -0500

    Creacion de Servicios & ApyKey
    Se creo el servicio y se agregaron la apy key para poder mandar la informacion de las noticias

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apyKey: '1ee65e0be9694447a9f12b301e8789c1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apyKey: '1ee65e0be9694447a9f12b301e8789c1'
 };
 
 /*
