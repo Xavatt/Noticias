f6f85fe0d43211386a060a959287dbf68c0b26c3
Xavier Guinto Rios <xavierguinto3@outlook.es>
2020-05-11 17:55:48 -0500

    Arreglos & Mas
    Se creo un arreglo para comenzar en tab2 para aplicar filtro en noticias

--- src/app/paginas/tab2/tab2.page.ts
+++ src/app/paginas/tab2/tab2.page.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
+
+
+  @ViewChild (IonSegment, {static:true}) s: IonSegment
+
+  categorias = [
+    'business',
+    'entertainment',
+    'general',
+    'health',
+    'science',
+    'sports',
+    'technology'
+  ];
+
+  ngOnInit()
+  {
+    this.s.value= this.categorias[0];
+  }
+
 
   constructor() {}
 
